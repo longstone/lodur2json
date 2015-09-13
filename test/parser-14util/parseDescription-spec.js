e552b82acde3c68974f98e35e790c5bdfd629710
longstone <git@longstone.ch>
2015-09-13 23:45:45 +0200

    add whitespace fix

--- test/parser-14util/parseDescription-spec.js
+++ test/parser-14util/parseDescription-spec.js
@@ -8,17 +8,23 @@ var parseDescription = require("../../parser/parser-14util/parseDescription");
 
 var oneGroup = "043 - 18:16 Uhr / KA5 / Brand: Ringstrasse, Brüttisellen";
 var twoGroups = "150 - 16:03 Uhr / KA2+KA3 / BMA: Rotbuchstrasse, Dübendorf";
+var tooMuchWhitespaces = '159 - 22:15 Uhr / Kdo / Wasser im Gebäude: Schlossstrasse, Dübendorf     ';
 describe("test description", function () {
     it('should parse "Brand: Ringstrasse, Brüttisellen"', function () {
         var actual = parseDescription(oneGroup);
         var expected = "Brand: Ringstrasse, Brüttisellen";
         assert.deepEqual(actual, expected);
-    })
-});
-describe("test description", function () {
+    });
+
     it('should parse "BMA: Rotbuchstrasse, Dübendorf"', function () {
         var actual = parseDescription(twoGroups);
         var expected = "BMA: Rotbuchstrasse, Dübendorf";
         assert.deepEqual(actual, expected);
-    })
+    });
+
+    it('should remove more than one whitespace', function(){
+       var actual = parseDescription(tooMuchWhitespaces);
+        var expected = 'Wasser im Gebäude: Schlossstrasse, Dübendorf';
+        assert.deepEqual(actual,expected);
+    });
 });
